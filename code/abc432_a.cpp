35ba8bc02d744f93f2a7dd7b37d06041d63a95be
tamfuyin <tamfuyin@gmail.com>
2025-11-16 15:13:09 +0800

    update abc432_a.cpp

--- code/abc432_a.cpp
+++ code/abc432_a.cpp
@@ -1,7 +1,5 @@
 #include<bits/stdc++.h>
 int main(){
-    int a[3];
-    std::cin>>a[0]>>a[1]>>a[2];
-    std::sort(a,a+3,std::greater<int>());
-    std::cout<<a[0]<<a[1]<<a[2];
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    int n;cin>>n;
 }
